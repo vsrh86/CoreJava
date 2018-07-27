9adeba2ea5c53328742bb61a6b418a1d092655c4
sureshv <sureshv@github.com>
2018-07-27 18:40:29 +0530

    Adding Sample1 Branch

--- TestGitHub/src/com/sss/Github/Sample.java
+++ TestGitHub/src/com/sss/Github/Sample.java
@@ -2,6 +2,9 @@ package com.sss.Github;
 
 public class Sample {
 public static void main(String[] args) {
-	System.out.println("Sample 2 added");
+	int a[]={1,2,4,6,10};
+	for(int i=0;i<a.length;i++){
+		System.out.println(a[i]);
+	}
 }
 }
