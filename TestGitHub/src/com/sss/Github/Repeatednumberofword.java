f88c76f592e7fb74861d8c6b2bd65ff1df29cd40
suresh <vsr86@gmail.com>
2019-01-27 22:48:56 +0530

    Repeat word commit=============

---
+++ TestGitHub/src/com/sss/Github/Repeatednumberofword.java
@@ -0,0 +1,20 @@
+package com.sss.Github;
+
+public class Repeatednumberofword {
+	public static void main(String[] args) {
+		String s="sureshvuppatala";
+		int count=0;
+		
+	char a[]=new char[s.length()];
+
+		for(int i=0;i<s.length();i++){
+			for(int j=i+1;j<s.length();j++){
+				if(a[i]==a[j]){
+					 count++;
+					System.out.println("the word repeated" +a[i]+count+"times");
+				}
+			}
+		}
+	}
+
+}
