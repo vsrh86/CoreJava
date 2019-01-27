b5aa0b79b7cff79cb62bb1db084a30772a6aa471
suresh <vsr86@gmail.com>
2019-01-27 22:37:45 +0530

    Countwordsin java commit

---
+++ TestGitHub/src/com/sss/Github/CountwordsinSentence.java
@@ -0,0 +1,14 @@
+package com.sss.Github;
+
+public class CountwordsinSentence {
+public static void main(String[] args) {
+	String sentence="SURESH";
+	String s=sentence.trim();	
+	int count=0;
+	for(int i=0;i<sentence.length();i++){
+		count++;
+		
+	}
+	System.out.println("noofwords==="+count);
+}
+}
