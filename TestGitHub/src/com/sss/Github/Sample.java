e9b77ba168f8e1f7771ee3d7a340285fb9a41455
LENOVO <vsrh86@gmail.com>
2022-01-09 10:34:45 +0545

    Added few lines

--- TestGitHub/src/com/sss/Github/Sample.java
+++ TestGitHub/src/com/sss/Github/Sample.java
@@ -13,7 +13,7 @@ public static void main(String[] args) {
 
 	int a[]={1,2,4,6,10};
 	for(int i=0;i<a.length;i++){
-		System.out.println(a[i]);
+		System.out.println("Adding few lines=========="+a[i]);
 	}
 
 }
